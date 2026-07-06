e08d65d69d2a495d5a9a47c67fac2b1947d19a08
watershermistonwayne981998-alt <watershermistonwayne981998@gmail.com>
2026-07-06 13:37:55 +0800

    Update Theme.kt

--- app/src/main/java/com/xiaozhao/assistant/ui/theme/Theme.kt
+++ app/src/main/java/com/xiaozhao/assistant/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
 
 private val LightColors = lightColorScheme(
     primary = Primary,
